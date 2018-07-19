afd86bd7e851cbaa8815f909f42bbe9fa1347329
Nerhbest <nerhbest@gmail.com>
2018-07-19 16:22:08 +0300

    Initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var gulp = require('gulp'),
-		pug = require('gulp-pug'),
+		fileinclude = require("gulp-file-include"),
 		sass = require('gulp-sass'),
 		concat = require('gulp-concat'),
 		plumber = require('gulp-plumber'),
@@ -14,8 +14,7 @@ var useref = require('gulp-useref'),
 		cssmin = require('gulp-clean-css'),
 		uglify = require('gulp-uglify'),
 		rimraf = require('rimraf'),
-		notify = require('gulp-notify'),
-		ftp = require('vinyl-ftp');
+		notify = require('gulp-notify');
 
 var paths = {
 			blocks: 'blocks/',
@@ -28,11 +27,13 @@ var paths = {
 		Developer tasks
 *********************************/
 
-//pug compile
-gulp.task('pug', function() {
-	return gulp.src([paths.blocks + '*.pug', '!' + paths.blocks + 'template.pug' ])
+//html compile
+gulp.task('html', function() {
+	return gulp.src(paths.blocks + 'index.html')
 		.pipe(plumber())
-		.pipe(pug({pretty: true}))
+		.pipe(fileinclude({
+			prefix: "@@"
+		}))
 		.pipe(gulp.dest(paths.devDir))
 		.pipe(browserSync.stream())
 });
@@ -63,7 +64,7 @@ gulp.task('scripts', function() {
 
 //watch
 gulp.task('watch', function() {
-	gulp.watch(paths.blocks + '**/*.pug', ['pug']);
+	gulp.watch(paths.blocks + '**/*.html', ['html']);
 	gulp.watch(paths.blocks + '**/*.sass', ['sass']);
 	gulp.watch(paths.blocks + '**/*.js', ['scripts']);
 });
@@ -110,31 +111,9 @@ gulp.task('fontsBuild', ['clean'], function() {
 		.pipe(gulp.dest(paths.outputDir + 'fonts/'));
 });
 
-//ftp
-gulp.task('send', function() {
-	var conn = ftp.create({
-		host:     '',
-		user:     '',
-		password: '',
-		parallel: 5
-	});
-
-	/* list all files you wish to ftp in the glob variable */
-	var globs = [
-		'build/**/*',
-		'!node_modules/**' // if you wish to exclude directories, start the item with an !
-	];
-
-	return gulp.src( globs, { base: '.', buffer: false } )
-		.pipe( conn.newer( '/' ) ) // only upload newer files
-		.pipe( conn.dest( '/' ) )
-		.pipe(notify("Dev site updated!"));
-
-});
-
 
 //default
-gulp.task('default', ['browser-sync', 'watch', 'pug', 'sass', 'scripts']);
+gulp.task('default', ['browser-sync', 'watch', 'html', 'sass', 'scripts']);
 
 //production
 gulp.task('prod', ['build', 'imgBuild', 'fontsBuild']);
